1a88e3d64ea14c2a3e8a4cc0b0bbbda06e9132e9
unknown <magzxia@gmail.com>
2022-01-25 15:10:42 -0500

    message

--- fs.py
+++ fs.py
@@ -5,3 +5,9 @@ class Folder:
         self.full_name = full_name
         self.files_count = files_count
         self.folders_count = folders_count
+
+class Files:
+    def __init__(self, id, display_name, size):
+        self.id = id
+        self.display_name = display_name
+        self.size = size
