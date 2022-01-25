643b038e9446fd284903704d805b2e88e05a547f
Milansuman <milansuman2016@gmail.com>
2022-01-25 23:10:13 +0530

    added more functionality

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+from wsgiref import headers
 import requests
 
 token = "2096~w2GsDFAC1Txmj3NGtf93Aq1zaL7UiIfpFBVMgFx3yNhi6qFbbiDoaSVmfKrS7WYP"
 
-def list_files(course_id="", folder_id="", content_type=""):
+#TODO: replace print statements with sane return statements
+def list_files(course_id="", folder_id="", content_type="", sort="name"):
     if course_id != "":
-        res = requests.get(f"https://gatech.instructure.com/api/v1/courses/{course_id}/files?content_types[]={content_type}", headers={"Authorization": f"Bearer {token}"})
+        res = requests.get(f"https://gatech.instructure.com/api/v1/courses/{course_id}/files?content_types[]={content_type}&sort={sort}", headers={"Authorization": f"Bearer {token}"})
         print(res.json())
     elif folder_id != "":
-        res = requests.get(f"https://gatech.instructure.com/api/v1/folders/{folder_id}/files?content_types[]={content_type}", headers={"Authorization": f"Bearer {token}"})
+        res = requests.get(f"https://gatech.instructure.com/api/v1/folders/{folder_id}/files?content_types[]={content_type}&sort={sort}", headers={"Authorization": f"Bearer {token}"})
         print(res.json())
 
+def list_all_folders(course_id=""):
+    res = requests.get(f"https://gatech.instructure.com/api/v1/courses/{course_id}/folders", headers={"Authorization": f"Bearer {token}"})
+    print(res.json())
+
 def main():
-    list_files(course_id="231224", content_type="image")
+    list_all_folders(course_id="231224")
 
 if __name__ == "__main__":
     main()
