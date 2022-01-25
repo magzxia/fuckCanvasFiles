8462ab153694595bf64751e992134d9dd0e17475
Milansuman <milansuman2016@gmail.com>
2022-01-25 22:49:16 +0530

    working prototype

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from wsgiref import headers
 import requests
 
 token = "2096~w2GsDFAC1Txmj3NGtf93Aq1zaL7UiIfpFBVMgFx3yNhi6qFbbiDoaSVmfKrS7WYP"
 
 def list_files(course_id="", folder_id=""):
     if course_id != "":
-        res = requests.post(f"https://gatech.instructure.com/api/v1/courses/{course_id}/files", headers=f"Authorization: Bearer {token}")
+        res = requests.post(f"https://gatech.instructure.com/api/v1/courses/{course_id}/files", headers={"Authorization": f"Bearer {token}"})
         print(res.json())
     elif folder_id != "":
-        res = requests.post(f"https://gatech.instructure.com/api/v1/folders/{folder_id}/files", headers=f"Authorization: Bearer {token}")
+        res = requests.post(f"https://gatech.instructure.com/api/v1/folders/{folder_id}/files", headers={"Authorization": f"Bearer {token}"})
         print(res.json())
 
 def main():
-    list_files(course_id="")
+    list_files(course_id="231224")
 
 if __name__ == "__main__":
     main()
