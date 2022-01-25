073a8c42562f4f28a509034a3687bcd2934a5d82
Milansuman <milansuman2016@gmail.com>
2022-01-25 23:03:20 +0530

    fixes

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import requests
 
 token = "2096~w2GsDFAC1Txmj3NGtf93Aq1zaL7UiIfpFBVMgFx3yNhi6qFbbiDoaSVmfKrS7WYP"
 
-def list_files(course_id="", folder_id=""):
+def list_files(course_id="", folder_id="", content_type=""):
     if course_id != "":
-        res = requests.post(f"https://gatech.instructure.com/api/v1/courses/{course_id}/files", headers={"Authorization": f"Bearer {token}"})
+        res = requests.get(f"https://gatech.instructure.com/api/v1/courses/{course_id}/files?content_types[]={content_type}", headers={"Authorization": f"Bearer {token}"})
         print(res.json())
     elif folder_id != "":
-        res = requests.post(f"https://gatech.instructure.com/api/v1/folders/{folder_id}/files", headers={"Authorization": f"Bearer {token}"})
+        res = requests.get(f"https://gatech.instructure.com/api/v1/folders/{folder_id}/files?content_types[]={content_type}", headers={"Authorization": f"Bearer {token}"})
         print(res.json())
 
 def main():
-    list_files(course_id="231224")
+    list_files(course_id="231224", content_type="image")
 
 if __name__ == "__main__":
     main()
