17678c54b9c9077abb5a8e9a4ee93552aa092135
Milansuman <milansuman2016@gmail.com>
2022-01-26 09:24:17 +0530

    first code

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from canvasapi import Canvas, folder, file
 
+api_url = "https://gatech.instructure.com/"
+api_key = "2096~w2GsDFAC1Txmj3NGtf93Aq1zaL7UiIfpFBVMgFx3yNhi6qFbbiDoaSVmfKrS7WYP"
 
 def main():
-    pass
+    canvas = Canvas(api_url, api_key)
+    course = canvas.get_course("231224")
+    for file_obj in course.get_files():
+        print(file_obj)
+
 
 if __name__ == "__main__":
     main()
