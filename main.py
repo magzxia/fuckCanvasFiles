eeeadf102603a4a4cbcce3343b05c1548f46ef32
Milansuman <milansuman2016@gmail.com>
2022-01-26 10:09:59 +0530

    new course

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from canvasapi import Canvas, folder, file
+from canvasapi import Canvas
 
 api_url = "https://gatech.instructure.com/"
 api_key = "2096~w2GsDFAC1Txmj3NGtf93Aq1zaL7UiIfpFBVMgFx3yNhi6qFbbiDoaSVmfKrS7WYP"
 
 def main():
     canvas = Canvas(api_url, api_key)
-    course = canvas.get_course("231224")
-    for file_obj in course.get_files():
-        print(file_obj)
+    course = canvas.get_course("284188")
+    files = []
+
+    for file in course.get_files():
+        if file not in files:
+            files.append(file)
+
+    
+        
+    
 
 
 if __name__ == "__main__":
